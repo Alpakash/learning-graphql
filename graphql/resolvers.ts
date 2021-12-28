ce7a3f55e05c06468e8fbeea619ccc283a13bdfa
Akash Soedamah <akash.soedamah@gmail.com>
2021-12-28 14:49:39 +0100

    Got graphql / apollo-server working with React (NextJS)

---
+++ graphql/resolvers.ts
@@ -0,0 +1,31 @@
+export const resolvers = {
+	Query: {
+		links: () => [
+			{
+				category: "Open Source",
+				description: "Fullstack React framework",
+				id: "8a9020b2-363b-4a4f-ad26-d6d55b51bqes",
+				imageUrl: "https://nextjs.org/static/twitter-cards/home.jpg",
+				url: "https://nextjs.com",
+				title: "Next.js",
+			},
+			{
+				category: "Open Source",
+				description: "Next Generation ORM for TypeScript and JavaScript",
+				id: "2a3121b2-363b-4a4f-ad26-d6c35b41bade",
+				imageUrl: "https://www.prisma.io/images/og-image.png",
+				url: "https://prisma.com",
+				title: "Prisma",
+			},
+			{
+				category: "Open Source",
+				description: "Utility-fist css framework",
+				id: "6a9122b2-363b-4a4f-ad26-d6c55b51baed",
+				imageUrl:
+					"https://tailwindcss.com/_next/static/media/twitter-large-card.85c0ff9e455da585949ff0aa50981857.jpg",
+				url: "https://tailwindcss.com",
+				title: "TailwindCSS",
+			}
+		],
+	},
+};
---
+++ graphql/schema.ts
@@ -0,0 +1,16 @@
+import { gql } from "apollo-server-micro";
+
+export const typeDefs = gql`
+	type Link {
+		id: ID
+		title: String
+		description: String
+		category: String
+		imageUrl: String
+		users: [String]
+	}
+
+	type Query {
+		links: [Link]!
+	}
+`;
